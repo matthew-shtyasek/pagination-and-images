9b689d0e8b6d5c707ed29ff6b22622dab64bc582
matthew <admin@mail.ru>
2023-03-04 09:38:46 +0500

    Добавили post_list.html и post_list_view

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from pages.models import Post
+
+
+def post_list_view(request):
+    posts = Post.objects.filter(published=True)
+    return render(request, 'pages/post_list.html', {'post_list': posts})
