3ba613ce76bf09daf4f9eab9734eb27eecd1b6da
matthew <admin@mail.ru>
2023-03-04 09:13:05 +0500

    Добавили шаблоны (root и image) и обработчик

--- images/views.py
+++ images/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from images.models import Image
+
+
 def images_list_view(request):
-    pass
+    image_list = Image.objects.all()
+    return render(request, 'images/images.html',
+                  context={'images': image_list})
